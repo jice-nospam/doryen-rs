0b1e6670d0e24dd9085459fc01b5e44637c95c05
jice <jice.nospam@gmail.com>
2019-07-12 16:04:01 +0200

    comment the shaders

--- src/doryen_fs.glsl
+++ src/doryen_fs.glsl
@@ -1,22 +1,28 @@
 precision mediump float;
-uniform sampler2D uFont;
-uniform sampler2D uAscii;
-uniform sampler2D uFront;
-uniform sampler2D uBack;
+uniform sampler2D uFont; // font texture
+uniform sampler2D uAscii; // console ascii code
+uniform sampler2D uFront; // console foreground color
+uniform sampler2D uBack; // console background color
+// used to convert ascii code into x,y character pos inside the font
 uniform float uFontCharsPerLine;
+// converts character pos inside font (0,0) - (16,16) into texture coord (0,0) - (1,1)
 uniform vec2 uFontCoef;
+// converts cell pos inside console (0,0) - (console_width,console_height) into texture coord (0,0) - (1,1)
+// console texture is not console_width x console_height but the closest power of 2 value
 uniform vec2 uTermCoef;
 
 in vec2 vTextureCoord;
 out vec4 FragColor;
 void main(){
-    vec2 pixPos = fract(vTextureCoord) * uFontCoef;
+    // address = coordinate in the console textures (front, back, ascii) between (0,0) and (1,1)
     vec2 address = floor(vTextureCoord) * uTermCoef + vec2(0.001, 0.001);
     vec4 ascii_vec = texture(uAscii, address);
     float ascii_code = (ascii_vec.r * 256.0) + (ascii_vec.g * 256.0 * 256.0);
     vec4 foreground = texture(uFront, address);
     vec4 background = texture(uBack, address);
     vec2 tchar = vec2(mod(floor(ascii_code), floor(uFontCharsPerLine)), floor(ascii_code / uFontCharsPerLine));
+    // where are we inside the cell / glyph
+    vec2 pixPos = fract(vTextureCoord) * uFontCoef;
     vec4 font_color = texture(uFont, tchar * uFontCoef + pixPos);
     FragColor=font_color.a * foreground * vec4(font_color.rgb,1.0) + (1.0 - font_color.a) * background;
 }
--- src/doryen_vs.glsl
+++ src/doryen_vs.glsl
@@ -3,6 +3,8 @@ in vec2 aTextureCoord;
 out vec2 vTextureCoord;
 uniform vec2 uTermSize;
 void main(void) {
+    // vertex position from (-1,-1) to (1,1)
     gl_Position = vec4(aVertexPosition.xy, 0.0, 1.0);
+    // texture coordinates from (0,0) to (console_width,console_height)
     vTextureCoord = aTextureCoord * uTermSize;
 }
